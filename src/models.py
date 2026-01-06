658a0e4e245d3cfa5e824ad83e05dc200960867a
diogodccoimbra24 <diogodccoimbra@gmail.com>
2026-01-06 01:19:32 +0100

    creating fuse model in models file

--- src/models.py
+++ src/models.py
@@ -1,8 +1,8 @@
-
-
+import keras
 import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.layers import Embedding, LSTM
+from keras.models import Model
 
 #Text encoder model
 def build_text_encoder(vocab_size, max_length=20):
@@ -40,5 +40,29 @@ def build_image_encoder():
 
 build_image_encoder().summary()
 
+#fusion model (between text encoder and image encoder)
+def build_fusion_model(vocab_size):
+
+    # Using directly the encoders
+    image_encoder = build_image_encoder()
+    text_encoder = build_text_encoder(vocab_size)
+
+    #Creating inputs for the encoders
+    image_input = keras.Input(shape = (224, 224, 3), name = 'image_input')
+    text_input = keras.Input(shape=(20, ), dtype='int32')
+
+
+    #Calling the functions of the encoders with the inputs
+    image_feature = image_encoder(image_input)
+    text_feature = text_encoder(text_input)
+
+    #Concatenation the outputs from the encoders
+    fused = keras.layers.concatenate([image_feature,text_feature], -1)
+
+    #returnin the model with the inputs and output
+    return Model(inputs = [image_input, text_input], outputs = fused)
+
+fusion_model = build_fusion_model(vocab_size= 8829)
+fusion_model.summary()
 
 
